59c95fb6e9325be63ae2f83744d92a7c366bdfd9
Sam Herrmann <webocular@gmail.com>
2016-12-12 22:49:23 -0500

    Use stderr for ping errors

--- ping/ping.go
+++ ping/ping.go
@@ -1,15 +1,15 @@
 package ping
 
+import "errors"
+
 // Run pings the specified address and returns nil
 // if a response is received. An error is returned
 // if no response is received.
 func Run(addr string, timeout int) (err error) {
 	cmd := command(addr, timeout)
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	if err := cmd.Wait(); err != nil {
-		return err
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return errors.New(string(out))
 	}
 	return nil
 }
