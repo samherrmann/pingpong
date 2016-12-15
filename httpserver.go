2209cf0692709cb876e76611f0a17497a364307a
Sam Herrmann <webocular@gmail.com>
2016-12-14 23:55:11 -0500

    Dividing main.go into multiple files
    No code change

---
+++ httpserver.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/samherrmann/pingpong/network"
+)
+
+//go:generate go run build/template/main.go
+
+// registerUI registers an HTTP handler function that presents the results
+// in a web user interface
+func registerUI(nb *network.NodesBuffer) error {
+	tpl, err := template.New("index.html").Parse(indexHTML)
+	if err != nil {
+		return err
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, &ViewModel{*interval, nb.Get(), version})
+	})
+	return nil
+}
+
+// registerAPI registers an HTTP handler function that provides  the results
+// in JSON format
+func registerAPI(nb *network.NodesBuffer) {
+	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(nb.Get())
+	})
+}
+
+func listenAndServe() error {
+	log.Println("Listening on port " + strconv.Itoa(*port))
+	return http.ListenAndServe(":"+strconv.Itoa(*port), nil)
+}
---
+++ version.go
@@ -0,0 +1,9 @@
+package main
+
+var (
+	// version is the version of this software,
+	// and should be initialized through the
+	// following build flags:
+	// go build -ldflags "-X main.version=version"
+	version = "latest"
+)
